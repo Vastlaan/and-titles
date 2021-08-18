fa4cea7a06dcdcb4acf4de5749e29754a3f1fc86
EAM <eam@eams-mbp.home>
2021-08-18 10:20:33 +0200

    Added request to add entry.

--- scripts.js
+++ scripts.js
@@ -13,6 +13,12 @@ async function renderCards(){
 
   const data = await getData()
 
+  const existingCards = document.getElementById("cards");
+
+  if (existingCards) {
+    existingCards.remove();
+  }
+
   const cards = document.createElement('div')
   cards.id='cards'
 
@@ -35,10 +41,23 @@ renderCards()
 const form = document.querySelector('#form')
 form.addEventListener('submit', submitForm)
 
-async function submitForm(){
+async function submitForm(event){
+  event.preventDefault();
+  
+  const name = document.getElementById("name_field").value;
+  const title = document.getElementById("title_field").value;
+
+  console.log(`name=${name}&title=${title}`);
 
   const response = await fetch('https://us-central1-and-business-card.cloudfunctions.net/add', {
     method: "POST",
-    
-  })
+    headers: {
+      "Content-Type": "multipart/form-data"
+    },
+    body: `name=${name}&title=${title}`
+  });
+
+  console.log(response);
+
+  renderCards();
 }
